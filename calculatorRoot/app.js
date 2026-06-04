0d30ee497f5b481e5f655e5b221b5fd2d0874ba2
Suraj <surajm3290@gmail.com>
2026-06-04 08:49:08 +0530

    update select and overall Design , below container aaded in structure and trying to add image in select

--- calculatorRoot/app.js
+++ calculatorRoot/app.js
@@ -4,10 +4,14 @@ const selectTo = document.querySelector("#selectTo");
 const btn = document.querySelector("#convert");
 const inputNum = document.querySelector("#fromInput");
 const outputNum = document.querySelector("#toOutput");
+const flagImage = document.querySelector(".FlagImg");
 let toConvert = 1;
 
+const flagBaseUrl = "https://flagsapi.com";
 for (let i in countryList) {
-    let optionfrom = new Option(countryList[i], i);
+    let img = `<img src="${flagBaseUrl}/${countryList[i]}/flat/64.png" alt="countryList[i]"> `;
+    let displayOption = `${img} ${countryList[i]}`
+    let optionfrom = new Option(displayOption, i);
     let optionto = new Option(countryList[i], i);
     selectFrom.add(optionfrom);
     selectTo.add(optionto);
@@ -22,21 +26,22 @@ let fromCode;
 let toCode;
 
 selectFrom.addEventListener("change", (evt) => {
-    fromCode = evt.target.value
+    fromCode = evt.target.value;
 })
 
 selectTo.addEventListener("change", (evt1) => {
-    toCode = evt1.target.value
+    toCode = evt1.target.value;
 })
-
-let URL = `https://api.frankfurter.dev/v2/rate/${fromCode}/${toCode}`;
 
 async function rate(){
     console.log("Getting data");
+    let URL = `https://api.frankfurter.dev/v2/rate/${fromCode}/${toCode}`;
     let response = await fetch(URL);
     let data = await response.json();
-    outputNum.value = Number(toConvert) * data.rate;
-    console.log(data.rate);
+    let result = data.rate;
+    outputNum.value = toConvert * result ;
+    console.log(result);
+    console.log(URL);
     }
 
 // {
@@ -48,6 +53,14 @@ async function rate(){
 
 btn.addEventListener('click', (evt) => {
     evt.preventDefault();
+    toConvert = Number(toConvert);
+    console.log(fromCode);
+    console.log(typeof fromCode);
+    console.log(toCode);
+    console.log(typeof toCode);
+    console.log(toConvert);
+    console.log(typeof toConvert);
+
     rate();
     
 });
